e85251fc78508d92902bb5fc44a52251dc1ea7e8
ZarevichMikhail <zarevichm@gmail.com>
2026-03-15 14:23:25 +0700

    Рефракторинг.

--- Lab2DoublyLinkedList/DoublyLinkedList.cpp
+++ Lab2DoublyLinkedList/DoublyLinkedList.cpp
@@ -3,63 +3,52 @@
 using namespace std;
 
 DoublyLinkedList::DoublyLinkedList() {
-
 	head_ = nullptr;
 	tail_ = nullptr;
-
 	size_ = 0;
 }
 
 
 DoublyLinkedList::~DoublyLinkedList() {
-
 	Node* current_node = head_;
 
 	while (current_node != nullptr) {
-
 		DeleteNode(current_node);
 	}
-
 }
 
 
-Node* DoublyLinkedList::GetHead() {
-
+const Node* DoublyLinkedList::GetHead() {
 	return head_;
 }
 
 
-Node* DoublyLinkedList::GetTail() {
-
+const Node* DoublyLinkedList::GetTail() {
 	return tail_;
 }
 
 
-int DoublyLinkedList::GetSize() {
-
+const int DoublyLinkedList::GetSize() {
 	return size_;
 }
 
 
-void DoublyLinkedList::PrintList() {
-
-
+const void DoublyLinkedList::PrintList() {
 	Node* current_node = head_;
 	
 	cout << "Элементы списка: " << endl;
-	while (current_node != nullptr) {
 
+	while (current_node != nullptr) {
 		cout << current_node->data_<< " ";
 		current_node = current_node->next_;
 	}
+
 	cout << endl;
 }
 
 
-
-
 void DoublyLinkedList::CheckRange(int size, int index, std::function<bool(int, int)> compare_mode) {
-
+	
 	if (index < 0) {
 		throw std::out_of_range("Индекс должен быть больше 0. ");
 	}
@@ -67,18 +56,19 @@ void DoublyLinkedList::CheckRange(int size, int index, std::function<bool(int, i
 		throw std::out_of_range("Слишком большой индекс. ");
 	}
 }
-bool DoublyLinkedList::CompareForCheckIndex(int size, int index) {
 
+
+bool DoublyLinkedList::CompareForCheckIndex(int size, int index) {
 	return size <= index;
 }
-bool DoublyLinkedList::CompareForAddElement(int size, int index) {
 
+
+bool DoublyLinkedList::CompareForAddElement(int size, int index) {
 	return size < index;
 }
 
 
 void DoublyLinkedList::AddElement(int index, int value) {
-
 	// Проверка корректности индекса
 	std::function<bool(int, int)> func;
 	func = [&](int a, int b) { return CompareForAddElement(a, b); };
@@ -88,9 +78,10 @@ void DoublyLinkedList::AddElement(int index, int value) {
 
 	// Вставка в начало списка
 	if (index == 0) {
-		// Если при этом список пустой
-		if (head_ == nullptr) {
 
+		// Если при этом список пустой
+		// Тогда новый узел будет и head и tail одновременно
+		if (head_ == nullptr) {
 			new_node->next_ = nullptr;
 			new_node->prev_ = nullptr;
 
@@ -110,7 +101,6 @@ void DoublyLinkedList::AddElement(int index, int value) {
 	}
 	// Вставка в середину списка
 	else if (index != size_){
-
 		Node* node_to_move = GetElementByIndex(index);
 
 		new_node->next_ = node_to_move;
@@ -121,7 +111,6 @@ void DoublyLinkedList::AddElement(int index, int value) {
 	}
 	// Вставка в конец списка
 	else {
-
 		tail_->next_ = new_node;
 		new_node->prev_ = tail_;
 		new_node->next_ = nullptr;
@@ -136,16 +125,13 @@ void DoublyLinkedList::AddElement(int index, int value) {
 
 
 void DoublyLinkedList::SetElement(int index, int value) {
-
 	Node* node = GetElementByIndex(index);
 
 	node->data_ = value;
-
 }
 
 
 Node* DoublyLinkedList::GetElementByIndex(int index) {
-
 	// Проверка корректности индекса
 	std::function<bool(int, int)> func;
 	func = [&](int a, int b) { return CompareForCheckIndex(a, b); };
@@ -153,11 +139,13 @@ Node* DoublyLinkedList::GetElementByIndex(int index) {
 
 	Node* current_node = head_;
 	int current_index = 0;
+
 	while (current_node != nullptr) {
 
 		if (current_index == index) {
 			return current_node;
 		}
+
 		current_index = current_index + 1;
 		current_node = current_node->next_;
 	}
@@ -167,7 +155,6 @@ Node* DoublyLinkedList::GetElementByIndex(int index) {
 
 
 void DoublyLinkedList::RemoveByIndex(int index) {
-
 	// GetElementByIndex сам проверит индекс
 	Node* node_to_delete = GetElementByIndex(index);
 
@@ -175,12 +162,10 @@ void DoublyLinkedList::RemoveByIndex(int index) {
 		DeleteNode(node_to_delete);
 
 	}
-
 }
 
 
 void DoublyLinkedList::RemoveByValue(int value) {
-
 	Node* current_node = head_;
 
 	while (current_node != nullptr) {
@@ -190,13 +175,12 @@ void DoublyLinkedList::RemoveByValue(int value) {
 		}
 		current_node = current_node->next_;
 	}
-
 }
 
 
 void DoublyLinkedList::DeleteNode(Node* node_to_delete) {
 
-	
+	// Удаление head
 	if (node_to_delete == head_) {
 		head_ = node_to_delete->next_;
 
@@ -224,21 +208,16 @@ void DoublyLinkedList::DeleteNode(Node* node_to_delete) {
 
 	// Удаление элемента в середине
 	else {
-
 		node_to_delete->prev_->next_ = node_to_delete->next_;
 		node_to_delete->next_->prev_ = node_to_delete->prev_;
 	}
 
-
 	delete node_to_delete;
 	size_ = size_ - 1;
-
-
 }
 
 
 int DoublyLinkedList::LinearSearch(int value) {
-
 	Node* current_node = head_;
 	int current_index = 0;
 
@@ -251,16 +230,17 @@ int DoublyLinkedList::LinearSearch(int value) {
 		current_node = current_node->next_;
 		current_index = current_index + 1;
 	}
-
 	return -1;
 }
 
 
 void DoublyLinkedList::Sort() {
-
+	// Если в списке один элемент, то он уже отсортирован.
+	// Если нет элементов, то сортировка тоже не требуется. 
 	if (size_ < 2) {
 		return; 
 	}
+
 	Node* current = head_;
 
 	while (current->next_ != nullptr) {
@@ -271,12 +251,9 @@ void DoublyLinkedList::Sort() {
 			// После того, как current и current->next обменяются
 			// большее число съедет вправо, а current->next - влево
 			// Чтобы сохранить место в списке нужно отдельно сохранить current->next
-
 			Node* future_left_node = current->next_;
 			SwapNeighborNodes(current, future_left_node);
 
-			
-			
 			// Если текущий узел не head, делает шаг назад
 			if (future_left_node->prev_ != nullptr) {
 				current = future_left_node->prev_;
@@ -287,7 +264,6 @@ void DoublyLinkedList::Sort() {
 		}
 		current = current->next_;
 	}
-
 }
 
 
@@ -315,6 +291,5 @@ void DoublyLinkedList::SwapNeighborNodes(Node* left, Node* right) {
 	right->next_ = left;
 	left ->prev_ = right;
 	left ->next_ = n;
-
 }
 
--- Lab2DoublyLinkedList/Node.cpp
+++ Lab2DoublyLinkedList/Node.cpp
@@ -1,11 +1,9 @@
 #include "Node.h"
 
 Node::Node(int initial_data) {
-
 	data_ = initial_data;
 	prev_ = nullptr;
 	next_ = nullptr;
-
 }
 
 
